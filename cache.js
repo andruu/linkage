6ae2180d11ac87879a44006e758f6394f8f8ad84
Andrew Weir <andru.weir@gmail.com>
2015-10-14 07:56:26 -0400

    Update cache#deletePattern

--- cache.js
+++ cache.js
@@ -1,7 +1,5 @@
-var env   = process.env.NODE_ENV || "development";
-var redis = require("redis");
-require('redis-delete-wildcard')(redis);
-var client = redis.createClient();
+var env    = process.env.NODE_ENV || "development";
+var client = require("redis").createClient();
 
 var set = function (key, value, callback) {
   key = env + ':' + key;
@@ -16,7 +14,17 @@ var get = function (key, callback) {
 };
 
 var deletePattern = function (pattern, callback) {
-  client.delwild(pattern, callback);
+  client.keys(pattern, function handleKeys (err, keys) {
+    if (err) {
+      return callback(err);
+    }
+
+    if (keys.length) {
+      client.del(keys, callback);
+    } else {
+      callback(null);
+    }
+  });
 };
 
 module.exports = {
